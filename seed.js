f2043326ed148925185f7a9470163f408ae7d615
Sam <samuel1008@gmail.com>
2016-04-04 10:27:43 -0700

    updated seed.js

--- seed.js
+++ seed.js
@@ -22,40 +22,40 @@ var sportsteams = [
 
 ];
 
-// db.Sportsteam.remove({}, function(err, authors) {
-//   console.log('removed all teams');
-//   db.Sportsteam.create(teams_list, function(err, teams){
-//     if (err) {
-//       console.log(err);
-//       return;
-//     }
-//     console.log('recreated all authors');
-//     console.log("created", teams.length, "teams");
+db.Sportsteam.remove({}, function(err, authors) {
+  console.log('removed all teams');
+  db.Sportsteam.create(teams_list, function(err, teams){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('recreated all authors');
+    console.log("created", teams.length, "teams");
 
 
-//     db.Sportsteam.remove({}, function(err, teams){
-//       console.log('removed all teams');
-//       teams_list.forEach(function (sportsteamData) {
-//         var sportsteam = new db.Sportsteam({
-//           title: sportsteamData.city,
-//           name: sportsteamData.name,
-//           championships: sportsteamData.championships
-//         });
-//         db.Sportsteam.findOne({name: sportsteamData.name}, function (err, foundTeam) {
-//           console.log('testing 123');
-//           if (err) {
-//             console.log(err);
-//             return;
-//           }
-//           sportsteam.name = foundTeam;
-//             sportsteam.save(function(err, savedTeam){
-//               if (err) {
-//                 return console.log(err);
-//               }
-//               console.log('saved ' + savedteam.name + ' by ' + foundAuthor.name);
-//           });
-//         });
-//       });
-//     });
-//   });
-// });
+    db.Sportsteam.remove({}, function(err, teams){
+      console.log('removed all teams');
+      teams_list.forEach(function (sportsteamData) {
+        var sportsteam = new db.Sportsteam({
+          title: sportsteamData.city,
+          name: sportsteamData.name,
+          championships: sportsteamData.championships
+        });
+        db.Sportsteam.findOne({name: sportsteamData.name}, function (err, foundTeam) {
+          console.log('testing 123');
+          if (err) {
+            console.log(err);
+            return;
+          }
+          sportsteam.name = foundTeam;
+            sportsteam.save(function(err, savedTeam){
+              if (err) {
+                return console.log(err);
+              }
+              console.log('saved ' + savedteam.name + ' by ' + foundAuthor.name);
+          });
+        });
+      });
+    });
+  });
+});
